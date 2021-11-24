cc6c8129d157310482dc0852d24f7d44b7979feb
furkandindar <fdindar1@binghamton.edu>
2021-11-24 16:11:01 +0300

    delete mirrored from webcam

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
     
         <div className="outer-div">
         <button className="webcam-wrapper" style={{zIndex:20}} onClick={handleClick}>Switch camera</button>
-          <Webcam className="webcam-wrapper" ref={webcamRef} mirrored={facingMode === FACING_MODE_USER ? true : false} videoConstraints={{
+          <Webcam className="webcam-wrapper" ref={webcamRef} videoConstraints={{
           ...videoConstraints,
           facingMode
         }}/>
